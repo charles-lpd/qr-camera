379ba58ce7f302c1469e959ba7cd4c719ba2a3f7
lpd0916 <1985413702@qq.com>
2023-06-21 16:36:24 +0800

    fix: update test

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/index.scss'
+import * as avro from 'avsc'
+const tagParser = avro.Type.forSchema({
+  type: 'record',
+  name: 'Tag',
+  fields: [
+    { name: 'name', type: 'string' },
+    { name: 'value', type: 'string' }
+  ]
+})
+console.log(tagParser, 'tagParser')
 
-createApp(App).use(router as any).mount('#app')
+createApp(App).use(router).mount('#app')
